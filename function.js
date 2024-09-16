e7ad4fbbf7bd2e1353ccee01af9b329f84516d10
Satish232021 <100710941+Satish232021@users.noreply.github.com>
2024-09-16 18:11:58 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,27 @@
+const imageContainerElement = document.querySelector('.image-container');
+const prevElement = document.getElementById('prev');
+const nextElement = document.getElementById('next');
+
+let x = 0;
+let timer;
+prevElement.addEventListener('click', () => {
+    x = x + 45;
+    clearTimeout(timer);
+    updateGallery();
+});
+nextElement.addEventListener('click', () => {
+    x = x - 45;
+    clearTimeout(timer);
+    updateGallery();
+});
+
+function updateGallery() {
+    imageContainerElement.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(() => {
+        x = x - 45;
+        updateGallery();
+    },2000);
+}
+
+updateGallery();
+
